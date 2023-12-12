59e645e3851a842e65e9cda557c22cb3c58642a0
KaanSecen <kaansecen03@gmail.com>
2023-12-12 15:10:28 +0100

    database seeder

---
+++ database/factories/ItemStudentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ItemStudent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ItemStudent>
+ */
+class ItemStudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+    protected $model = ItemStudent::class;
+
+    public function definition(): array
+    {
+        return [
+            'student_id' => $this->faker->numberBetween(1, 100),
+            'item_id' => $this->faker->numberBetween(1, 50),
+            'is_active' => $this->faker->boolean,
+        ];
+    }
+}

97f29366f79db9ce43d0b2f8dcb8f0d1a7da7278
KaanSecen <kaansecen03@gmail.com>
2024-01-09 13:27:48 +0100

    added active item inventory

---
+++ app/GraphQL/Queries/Item/ActiveItemStudentQuery.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\GraphQL\Queries\Item;
+
+use App\Models\Item;
+use App\Models\ItemStudent;
+use GraphQL\Error\UserError;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+class ActiveItemStudentQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'item_inventory',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Item');
+    }
+
+    public function args(): array
+    {
+        return [
+            'student_id' => [
+                'name' => 'student_id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ]
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        //return only active student item and return that item model
+        $itemStudent = ItemStudent::where('student_id', $args['student_id'])->where('is_active', true)->first();
+
+
+        if(!$itemStudent) {
+            throw new UserError('No active item found for this student.');
+        }
+
+        return Item::where('id', $itemStudent->item_id)->first();
+    }
+}

1826ef50735ad2777fd2bb8d41c58c20b87bcf69
KaanSecen <kaansecen03@gmail.com>
2024-01-19 13:23:19 +0100

    feat: item update fixed

--- app/GraphQL/Mutations/ItemStudent/ItemStudentUpdateActive.php
+++ app/GraphQL/Mutations/ItemStudent/ItemStudentUpdateActive.php
@@ -37,36 +37,15 @@ class ItemStudentUpdateActive extends Query
         $itemStudent = ItemStudent::findOrFail($args['id']);
 
         if ($itemStudent->is_active == 1) {
-
-            // Check if the student has any other active item in that category if not then throw error
-            $student = Student::findOrFail($itemStudent->student_id);
-
-            $category_id = Item::where('id', $itemStudent->item_id)->value('category_id');
-
-            $otherActiveItems = ItemStudent::where('student_id', $student->id)
-                ->whereIn('item_id', Item::where('category_id', $category_id)->pluck('id'))
-                ->where('is_active', 1)
-                ->get();
-
-            if (count($otherActiveItems) == 1) {
-                throw new UserError('This is the only active item for this student in this category. Please activate another item in this category before deactivating this item.' . $itemStudent->item_id);
-            }
-
-
-            $itemStudent->is_active = 0;
+            throw new UserError('This item is already active for this student.');
         }
-        else {
-            $itemStudent->is_active = 1;
 
-            $student = Student::findOrFail($itemStudent->student_id);
+        // Deactivate all other items for the student
+        ItemStudent::where('student_id', $itemStudent->student_id)
+            ->update(['is_active' => 0]);
 
-            $category_id = Item::where('id', $itemStudent->item_id)->value('category_id');
-
-            ItemStudent::where('student_id', $student->id)
-                ->whereIn('item_id', Item::where('category_id', $category_id)->pluck('id'))
-                ->update(['is_active' => 0]);
-
-        }
+        // Activate the selected item
+        $itemStudent->is_active = 1;
         $itemStudent->save();
 
         return $itemStudent;
